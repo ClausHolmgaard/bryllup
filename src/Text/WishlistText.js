4dedf71910f95a3fa8ac188e86949c179c2b81b2
ClausHolmgaard <github@clausnet.dk>
2021-05-15 10:37:36 +0200

    wishlist move and update

--- src/Text/WishlistText.js
+++ src/Text/WishlistText.js
@@ -14,4 +14,48 @@ const lessText = new Map();
 lessText.set('english', 'Click for less');
 lessText.set('dansk', 'Klik for mindre');
 
-export {titleText, contactText, moreText, lessText};
+const listOfWishes = new Map();
+listOfWishes.set('english', [
+    ['Philips HD9650/90 Airfryer XXL Avance', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/frituregryder-og-airfryer/HD965090/philips-avance-collection-airfryer-xxl-hd9650-90?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
+    ['Greenpan Premier stegepande 30cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405349'],
+    ['Greenpan Premier stegepande 24cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405347'],
+    ['Greenpan Premier Wok', 'https://www.imerco.dk/greenpan-premiere-wok?id=100405351'],
+    ['Scanpan bradepande 5 liter', 'https://www.kop-kande.dk/scanpan-classic-bradepande-5-ltr-39x27-cm-083421353214'],
+    ['Dyson V11', 'https://www.elgiganten.dk/product/husholdning/stovsuger-og-rengoring/179558/dyson-v11-2020-torque-drive-extra-ledningslos-stovsuger'],
+    ['WMF Steak Bestiksæt - 12 dele', 'https://www.imerco.dk/wmf-steak-bestiksaet-12-dele?id=100023195'],
+    ['Kenwood Chef XL KVL4100S', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/kokkenmaskiner/KVL4100S/kenwood-chef-xl-kokkenmaskine-kvl4100s-solv-tank-testvinder?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
+    ['Gavekort til wellness ophold', ''],
+    ['Gavekort til gourmetophold', ''],
+    ['Gavekort til slotsophold', ''],
+    ['Træningsbænk - skrå - 300 kg', 'https://shop.getbig.dk/semi-pro-skraabaenk-ms-l101'],
+    ['Gavekort til træningsudstyr ved LC Gear', ''],
+    ['Gavekort til træningsudstyr ved Rogue Europe', ''],
+    ["Chef's Choice CC-1520 knivsliber", 'https://www.skousen.dk/koekkenudstyr/koekkenudstyr/knivsliber%20/product/chef-s-choice-1520/?utm_source=adtraction&utm_medium=affiliate&utm_campaign=Content&utm_campaign=1251558299&at_gd=1BC8BB0451B9D1036763767671C76203C019F9DD'],
+    ['DeLonghi Elkedel Active Line', 'https://www.proshop.dk/Elkedel/DeLonghi-Elkedel-Active-Line-Intens-sort-2000-W/2711780?paid=26990&pacid=609e72c0481eb4.31338448&utm_source=partner-ads&utm_medium=cpc&utm_campaign=26990'],
+    ['Luigi Bormioli Aero hvidvinsglas 32,5 cl.', 'https://www.imerco.dk/luigi-bormioli-aero-hvidvinsglas-6-stk?id=100325648'],
+    ['Luigi Bormioli Aero rødvinsglas 48 cl', 'https://www.imerco.dk/luigi-bormioli-aero-roedvinsglas-6-stk?id=100325646'],
+    ['Luigi Bormioli Aero Vandglas', 'https://www.imerco.dk/luigi-bormioli-aero-vandglas-6-stk?id=100325650']
+])
+listOfWishes.set('dansk', [
+    ['Philips HD9650/90 Airfryer XXL Avance', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/frituregryder-og-airfryer/HD965090/philips-avance-collection-airfryer-xxl-hd9650-90?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
+    ['Greenpan Premier stegepande 30cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405349'],
+    ['Greenpan Premier stegepande 24cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405347'],
+    ['Greenpan Premier Wok', 'https://www.imerco.dk/greenpan-premiere-wok?id=100405351'],
+    ['Scanpan bradepande 5 liter', 'https://www.kop-kande.dk/scanpan-classic-bradepande-5-ltr-39x27-cm-083421353214'],
+    ['Dyson V11', 'https://www.elgiganten.dk/product/husholdning/stovsuger-og-rengoring/179558/dyson-v11-2020-torque-drive-extra-ledningslos-stovsuger'],
+    ['WMF Steak Bestiksæt - 12 dele', 'https://www.imerco.dk/wmf-steak-bestiksaet-12-dele?id=100023195'],
+    ['Kenwood Chef XL KVL4100S', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/kokkenmaskiner/KVL4100S/kenwood-chef-xl-kokkenmaskine-kvl4100s-solv-tank-testvinder?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
+    ['Gavekort til wellness ophold', ''],
+    ['Gavekort til gourmetophold', ''],
+    ['Gavekort til slotsophold', ''],
+    ['Træningsbænk - skrå - 300 kg', 'https://shop.getbig.dk/semi-pro-skraabaenk-ms-l101'],
+    ['Gavekort til træningsudstyr ved LC Gear', ''],
+    ['Gavekort til træningsudstyr ved Rogue Europe', ''],
+    ["Chef's Choice CC-1520 knivsliber", 'https://www.skousen.dk/koekkenudstyr/koekkenudstyr/knivsliber%20/product/chef-s-choice-1520/?utm_source=adtraction&utm_medium=affiliate&utm_campaign=Content&utm_campaign=1251558299&at_gd=1BC8BB0451B9D1036763767671C76203C019F9DD'],
+    ['DeLonghi Elkedel Active Line', 'https://www.proshop.dk/Elkedel/DeLonghi-Elkedel-Active-Line-Intens-sort-2000-W/2711780?paid=26990&pacid=609e72c0481eb4.31338448&utm_source=partner-ads&utm_medium=cpc&utm_campaign=26990'],
+    ['Luigi Bormioli Aero hvidvinsglas 32,5 cl.', 'https://www.imerco.dk/luigi-bormioli-aero-hvidvinsglas-6-stk?id=100325648'],
+    ['Luigi Bormioli Aero rødvinsglas 48 cl', 'https://www.imerco.dk/luigi-bormioli-aero-roedvinsglas-6-stk?id=100325646'],
+    ['Luigi Bormioli Aero Vandglas', 'https://www.imerco.dk/luigi-bormioli-aero-vandglas-6-stk?id=100325650']
+])
+
+export {titleText, contactText, moreText, lessText, listOfWishes};
--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -6,41 +6,7 @@ import Box from '@material-ui/core/Box';
 import { Typography } from '@material-ui/core';
 
 import { Context } from '../function/Store';
-import {titleText, contactText, moreText, lessText} from '../Text/WishlistText';
-
-const listOfWishes = new Map();
-listOfWishes.set('english', [
-    ['Philips HD9650/90 Airfryer XXL Avance', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/frituregryder-og-airfryer/HD965090/philips-avance-collection-airfryer-xxl-hd9650-90?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
-    ['Greenpan Premier stegepande 30cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405349'],
-    ['Greenpan Premier stegepande 24cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405347'],
-    ['Greenpan Premier Wok', 'https://www.imerco.dk/greenpan-premiere-wok?id=100405351'],
-    ['Scanpan bradepande 5 liter', 'https://www.kop-kande.dk/scanpan-classic-bradepande-5-ltr-39x27-cm-083421353214'],
-    ['Dyson V11', 'https://www.elgiganten.dk/product/husholdning/stovsuger-og-rengoring/179558/dyson-v11-2020-torque-drive-extra-ledningslos-stovsuger'],
-    ['WMF Steak Bestiksæt - 12 dele', 'https://www.imerco.dk/wmf-steak-bestiksaet-12-dele?id=100023195'],
-    ['Kenwood Chef XL KVL4100S', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/kokkenmaskiner/KVL4100S/kenwood-chef-xl-kokkenmaskine-kvl4100s-solv-tank-testvinder?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
-    ['Gavekort til wellness ophold', ''],
-    ['Gavekort til gourmetophold', ''],
-    ['Gavekort til slotsophold', ''],
-    ['Træningsbænk - skrå - 300 kg', 'https://shop.getbig.dk/semi-pro-skraabaenk-ms-l101'],
-    ['Gavekort til træningsudstyr ved LC Gear', ''],
-    ['Gavekort til træningsudstyr ved Rogue Europe', '']
-])
-listOfWishes.set('dansk', [
-    ['Philips HD9650/90 Airfryer XXL Avance', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/frituregryder-og-airfryer/HD965090/philips-avance-collection-airfryer-xxl-hd9650-90?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
-    ['Greenpan Premier stegepande 30cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405349'],
-    ['Greenpan Premier stegepande 24cm', 'https://www.imerco.dk/greenpan-premiere-stegepande?id=100405347'],
-    ['Greenpan Premier Wok', 'https://www.imerco.dk/greenpan-premiere-wok?id=100405351'],
-    ['Scanpan bradepande 5 liter', 'https://www.kop-kande.dk/scanpan-classic-bradepande-5-ltr-39x27-cm-083421353214'],
-    ['Dyson V11', 'https://www.elgiganten.dk/product/husholdning/stovsuger-og-rengoring/179558/dyson-v11-2020-torque-drive-extra-ledningslos-stovsuger'],
-    ['WMF Steak Bestiksæt - 12 dele', 'https://www.imerco.dk/wmf-steak-bestiksaet-12-dele?id=100023195'],
-    ['Kenwood Chef XL KVL4100S', 'https://www.elgiganten.dk/product/husholdning/kokkenudstyr/kokkenmaskiner/KVL4100S/kenwood-chef-xl-kokkenmaskine-kvl4100s-solv-tank-testvinder?utm_id=91g3i1&utm_source=partnerads&utm_medium=affiliate&utm_campaign=b2c-loc-partnerads-running&utm_content=all'],
-    ['Gavekort til wellness ophold', ''],
-    ['Gavekort til gourmetophold', ''],
-    ['Gavekort til slotsophold', ''],
-    ['Træningsbænk - skrå - 300 kg', 'https://shop.getbig.dk/semi-pro-skraabaenk-ms-l101'],
-    ['Gavekort til træningsudstyr ved LC Gear', ''],
-    ['Gavekort til træningsudstyr ved Rogue Europe', '']
-])
+import {titleText, contactText, moreText, lessText, listOfWishes} from '../Text/WishlistText';
 
 const Wishlist = () => {
     const { language } = useContext(Context)
